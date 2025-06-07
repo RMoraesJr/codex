99c09bffe60d8ed8a24f924e74619ed2611c5561
RMoraesJr <82686867+RMoraesJr@users.noreply.github.com>
2025-06-07 02:48:46 -0600

    Update on style, script console, html.
    Console do Script tem notas das aulas de java script, adicionei um pouco de estilo no css, e adicionei informações ao HTML como due date e Priority

--- script.js
+++ script.js
@@ -27,3 +27,104 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+function calcpct(n1, n2) {
+    return (n2 / n1) * 100;
+}
+
+    let x = 40;
+    let y = 10;
+    let pct = calcpct(x, y);
+    console.log(`${pct}% de ${x} é ${y}`);
+
+function maiorDeIdade(idade) {
+    if (idade >= 18) {  
+        return true; // É maior de idade
+    } else { 
+        return false; // É menor de idade
+    }
+}
+
+function validacao(usuario, senha) {
+    if (usuario === 'pedro' && senha === '123') {
+        return true; // Acesso permitido
+    } else {
+        return false; // Acesso negado
+    }
+}
+
+let usuario = 'pedro';
+let senha = '123';
+if (validacao(usuario, senha)) {
+    console.log('Acesso permitido'); // True
+} else {
+    console.log('Acesso negado'); // False
+}
+
+let idade = 10;
+if (maiorDeIdade(idade)) {
+    console.log('Você é maior de idade'); // True
+} else {
+    console.log('Você é menor de idade'); // False
+}
+
+
+function somar(n1, n2) {
+ let resultado = n1 + n2;
+    console.log(`A soma de ${n1} e ${n2} é: ${resultado}`);
+    return resultado;
+}
+
+somar(5, 10);
+
+
+/* Modulo 1 - Variáveis, Tipos de Dados e Operadores
+let x = 2;
+let y = 3;
+let r = 13;
+let z = x + y + r;
+let nome = 'Renato';
+let sobrenome = 'Moraes';
+console.log(nome + ' ' + sobrenome);
+
+let nomecompleto = nome+` `+sobrenome;
+console.log(`O seu nome completo é: ${nomecompleto}`);
+console.log(`A soma de ${x} e ${y} é ${z}`);
+if (z >= 18) {
+    console.log('Voce é maior de idade');
+} // < Menor
+// > Maior
+// == Igual
+// === Igual estritamente
+// != Diferente
+// !== Diferente estritamente
+// >= Maior ou igual
+// <= Menor ou igual
+// && E
+// || Ou
+// ! Não
+*/
+
+/*
+let x = 10;
+let y = 5;
+console.log(x > y);
+
+let w = "10";
+let z = 10;
+console.log(w === z);
+
+let a = 10;
+let b = 5;
+console.log(a != b); 
+
+
+let precocarne = 40.3;
+
+if (precocarne <= 45) 
+    {
+    console.log('O preço da carne está barato');
+} else {
+    console.log('O preço da carne está caro'); 
+ }
+*/
