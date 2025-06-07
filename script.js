4d2aa3570ce4f0672b439cdb4ab4f93d82869ec8
RMoraesJr <82686867+RMoraesJr@users.noreply.github.com>
2025-06-06 22:01:43 -0600

    Add basic HTML5 site

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('click-me');
+    button.addEventListener('click', () => {
+        alert('Botão clicado!');
+    });
+});
